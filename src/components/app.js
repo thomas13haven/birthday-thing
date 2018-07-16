9f984696b3ce6a1cdad5e7d07f168f2aa7e54e1c
Jordan Hudgens <jordan@devcamp.com>
2018-07-16 13:46:12 -0600

    added a third party picker compnent and added it to my application

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import Picker from './picker';
 
 export default class App extends Component {
   render() {
     return (
       <div className="grid">
+      <h2 className="grid__title">Birthday Countdown </h2>
+
+      <div className="grid__skew-dark-one"> </div>
+      <div className="grid__skew-dark-two"> </div>
+      <div className="grid__skew-dark-three"> </div>
+
+      
         <div className="grid__skew-light-one"> </div>
         <div className="grid__skew-light-two"> </div>
-        <div className="grid__skew-light-three"> </div>
+        <div className="grid__skew-light-three"> </div>   
         
+        <Picker/>
       </div>
     );
   }
